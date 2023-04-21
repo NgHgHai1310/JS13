26498b14aa5b6907b49e728cf3928f8e446e9ec0
NgHgHai1310 <zzagueroz1102@gmail.com>
2023-04-21 09:16:09 +0700

    zz

---
+++ css/photo-viewer.js
@@ -0,0 +1,59 @@
+var request;
+var $current;
+var cache = {};
+var $frame = $('#photo-viewer');
+var $thumbs = $('.thumb');
+
+function crossfade($img) {
+    if ($current) {
+        $scurrent.stop().fadeOut('slow');
+    }
+    $img.css({
+        marginLeft: -$img.width() /2,
+        margintop: -$img.height() /2
+    });
+
+    $img.stop().fadeTo('slow', 1);
+    $current = $img;
+}
+$(document).on('click', '.thumb', function(e){
+    var $img,
+        src = this.href;
+        request = src;
+    e.preventDefault();
+    $thumbs.removeClass('active');
+    $(this).addClass('active');
+
+    if (cache.hasOwnProperty(src)){
+        if (cache[src].isLoading === false) {
+            crossfade(cache[src].$img);
+        }
+    } else {
+        $img = $('<img/>');
+        cache[src] = {
+            $img: $img,
+            isLoaDing: True
+        };
+        //
+        $img.on('load', function(){
+            $img.hide();
+            //
+            $frame.removeClass('is-loading').append($img);
+            cache[src].isLoading = false;
+            //
+            if (request === src){
+                crossfade($img);
+            }
+        });
+
+        $frame.addClass('is-loading');
+
+        $img.attr({
+            'src': src,
+            'alt': this.title || ''
+        });
+    }
+});
+
+//
+$('.thumb').eq(0).click();
